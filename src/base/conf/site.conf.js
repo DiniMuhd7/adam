73c1b7f11a19c065e0492f1113194bf1ba23e4ab
Deenyz <deenyzlastname@gmail.com>
2019-08-27 14:32:41 +0100

    deploy

--- src/base/conf/site.conf.js
+++ src/base/conf/site.conf.js
@@ -64,7 +64,7 @@ if (env === 'development') {
 } else {
   Ssl = true;
   Protocol = Ssl ? 'https://' : 'http://';
-  HostName = 'pablomagaz.com';
+  HostName = 'floating-tundra-89588.herokuapp.com';
   ServerUrl = `${ Protocol }${ HostName }`;
   SiteUrl = `${ ServerUrl }`;
   clientSecret = '113542417eed';
