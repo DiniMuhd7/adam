5ac5b28e91c8a812b83abd7a1286b10918d890e4
Deenyz <deenyzlastname@gmail.com>
2019-08-27 22:27:39 +0100

    Updates

--- enviroment/index.js
+++
@@ -1,7 +0,0 @@
-import { env } from 'base';
-import * as development from './development';
-import * as production from './production';
-
-const envConf = env === 'development' ? production : development;
-
-export default envConf;
--- templates/index.js
+++
@@ -1,6 +0,0 @@
-import main from './main';
-import blog from './blog';
-import post from './post';
-import tag from './tag';
-
-export { main, blog, post, tag };
